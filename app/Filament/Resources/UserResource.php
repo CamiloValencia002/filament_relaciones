6d88020b6e01f9d21a876dadbc3d21e136b936b3
INSTRUCTOR <INSTRUCTOR@PERAPRCSAFSD499.FormacionSena.RED>
2024-05-31 15:39:49 -0500

    Filament 100% BASE

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -39,6 +39,12 @@ class UserResource extends Resource
                     ->password()
                     ->required()
                     ->maxLength(255),
+                Forms\Components\Select::make('roles')
+                    ->relationship('roles','name')
+                    ->multiple()
+                    ->preload()
+                    ->searchable()
+
             ]);
     }
 
